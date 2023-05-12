6c33976af65fa2cac84019e0958a41f71f699b84
TIMOTHY010 <133240288+TIMOTHY010@users.noreply.github.com>
2023-05-12 23:52:51 +0100

    Update project1.java

--- project1.java
+++ project1.java
@@ -52,3 +52,56 @@ public class KineticEnergyGUI {
       });
    }
 }
+import java.util.Scanner;
+
+public class Force {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      double mass, acceleration, force;
+      
+      System.out.print("Enter mass (kg): ");
+      mass = sc.nextDouble();
+      
+      System.out.print("Enter acceleration (m/s^2): ");
+      acceleration = sc.nextDouble();
+      
+      force = mass * acceleration;
+      
+      System.out.println("Force = " + force + " N");
+   }
+}
+import javax.swing.*;
+
+public class ForceGUI {
+   public static void main(String[] args) {
+      JFrame frame = new JFrame("Force Calculator");
+      JPanel panel = new JPanel();
+      JLabel massLabel = new JLabel("Enter mass (kg):");
+      JTextField massField = new JTextField(10);
+      JLabel accelerationLabel = new JLabel("Enter acceleration (m/s^2):");
+      JTextField accelerationField = new JTextField(10);
+      JButton calculateButton = new JButton("Calculate");
+      JLabel resultLabel = new JLabel("Force = ");
+      JTextField resultField = new JTextField(10);
+      resultField.setEditable(false);
+      
+      panel.add(massLabel);
+      panel.add(massField);
+      panel.add(accelerationLabel);
+      panel.add(accelerationField);
+      panel.add(calculateButton);
+      panel.add(resultLabel);
+      panel.add(resultField);
+      
+      frame.add(panel);
+      frame.pack();
+      frame.setVisible(true);
+      
+      calculateButton.addActionListener(event -> {
+         double mass = Double.parseDouble(massField.getText());
+         double acceleration = Double.parseDouble(accelerationField.getText());
+         double force = mass * acceleration;
+         resultField.setText(Double.toString(force) + " N");
+      });
+   }
+}
