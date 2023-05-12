77cb74291863027e473fffda10b9e7f9d7d306aa
Domshak007 <132787417+Domshak007@users.noreply.github.com>
2023-05-12 23:48:31 +0100

    Update project1.java

--- project1.java
+++ project1.java
@@ -17,3 +17,38 @@ public class KineticEnergy {
         System.out.println("Kinetic Energy = " + kineticEnergy + " J");
     }
 }
+import javax.swing.*;
+
+public class KineticEnergyGUI {
+   public static void main(String[] args) {
+      JFrame frame = new JFrame("Kinetic Energy Calculator");
+      JPanel panel = new JPanel();
+      JLabel massLabel = new JLabel("Enter mass (kg):");
+      JTextField massField = new JTextField(10);
+      JLabel velocityLabel = new JLabel("Enter velocity (m/s):");
+      JTextField velocityField = new JTextField(10);
+      JButton calculateButton = new JButton("Calculate");
+      JLabel resultLabel = new JLabel("Kinetic energy = ");
+      JTextField resultField = new JTextField(10);
+      resultField.setEditable(false);
+      
+      panel.add(massLabel);
+      panel.add(massField);
+      panel.add(velocityLabel);
+      panel.add(velocityField);
+      panel.add(calculateButton);
+      panel.add(resultLabel);
+      panel.add(resultField);
+      
+      frame.add(panel);
+      frame.pack();
+      frame.setVisible(true);
+      
+      calculateButton.addActionListener(event -> {
+         double mass = Double.parseDouble(massField.getText());
+         double velocity = Double.parseDouble(velocityField.getText());
+         double kineticEnergy = 0.5 * mass * Math.pow(velocity, 2);
+         resultField.setText(Double.toString(kineticEnergy) + " J");
+      });
+   }
+}
